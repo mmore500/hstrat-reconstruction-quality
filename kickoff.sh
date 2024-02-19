62084646583b0659a5ff74c6006c0d419701a594
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2024-02-18 21:20:22 -0500

    Filter for only polytomies

--- kickoff.sh
+++ kickoff.sh
@@ -10,7 +10,7 @@ for annotation_size_bits in 64 256; do
 for differentia_width_bits in 1; do
 for stratum_retention_algo in "old-steady" "old-tilted" "new-hybrid" "new-steady" "new-tilted"; do
 
-for condition in "export num_islands=1 num_niches=1 tournament_size=2" "export num_islands=64 num_niches=8 tournament_size=2"; do
+for condition in "export num_islands=1 num_niches=1 tournament_size=2" "export num_islands=1 num_niches=1 tournament_size=1" "export num_islands=4 num_niches=2 tournament_size=2" "export num_islands=64 num_niches=8 tournament_size=2"; do
 for scale in "export population_size=1024 num_generations=10000" "export population_size=65536 num_generations=100000"; do
 
 
