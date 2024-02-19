94b9a14d19aba24a10267859a4ff2be4cd8bcac5
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2024-02-18 22:08:49 -0500

    Bump notebook pin

--- experiment.slurm.sh
+++ experiment.slurm.sh
@@ -107,7 +107,7 @@ mkdir -p "${WORKDIR}"
 ################################################################################
 echo Run experiment
 ################################################################################
-python3 -m papermill --cwd "${WORKDIR}" "https://raw.githubusercontent.com/mmore500/hstrat-reconstruction-quality/f95dbc8d372da8877b13d21b66e1d5073c8cdc4a/reconstruction-quality-experiment.ipynb" "${WORKDIR}/${NAME}+replicate=${replicate}+ext=.ipynb"
+python3 -m papermill --cwd "${WORKDIR}" "https://raw.githubusercontent.com/mmore500/hstrat-reconstruction-quality/096627fa5ec1286f603c0c7397cd1b2be93780ee/reconstruction-quality-experiment.ipynb" "${WORKDIR}/${NAME}+replicate=${replicate}+ext=.ipynb"
 
 ################################################################################
 echo Finished
