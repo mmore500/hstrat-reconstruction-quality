65e963281f1f88ae14c2dd4952256b9db9bee325
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2024-03-10 12:11:59 -0400

    Rename new -> surf, old -> col

--- kickoff.sh
+++ kickoff.sh
@@ -17,7 +17,7 @@ echo "SBATCH_DIR ${SBATCH_DIR}"
 for num_generations in 10000 100000; do
 for scope in "export population_size=4096 downsample=500" "export population_size=65536 downsample=500" "export population_size=65536 downsample=8000"; do
 for instrumentation in "export annotation_size_bits=32 differentia_width_bits=1" "export annotation_size_bits=64 differentia_width_bits=1" "export annotation_size_bits=256 differentia_width_bits=1" "export annotation_size_bits=256 differentia_width_bits=8"; do
-for stratum_retention_algo in "new-steady" "new-tilted" "new-hybrid" "old-steady" "old-tilted"; do
+for stratum_retention_algo in "surf-steady" "surf-tilted" "surf-hybrid" "col-steady" "col-tilted"; do
 
 for condition in "export num_islands=1 num_niches=1 tournament_size=2" "export num_islands=1 num_niches=1 tournament_size=1" "export num_islands=4 num_niches=2 tournament_size=2" "export num_islands=64 num_niches=8 tournament_size=2"; do
 
