fc82c3889d7fdefcfac10c41be0d98aaa7abd9e9
mmore500 <mmore500.login@gmail.com>
2024-03-18 16:42:13 +0000

    Deploying to binder from @ mmore500/hstrat-reconstruction-quality@40a241dd560c2091833815bb2d9b5072eb0e4ade 🚀

---
+++ collate-inner_nodes-dsamp.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+set -e
+set -o nounset
+
+export RUNMODE="${1}"
+echo "RUNMODE ${RUNMODE}"
+
+################################################################################
+echo Log context
+################################################################################
+echo "date $(date)"
+echo "hostname $(hostname)"
+echo "job ${SLURM_JOB_ID:-none}"
+echo "user ${USER}"
+
+ln -s "${HOME}/scratch" "/mnt/scratch/${USER}/" || :
+
+echo "num data files?"
+find "${HOME}/scratch/hstrat-reconstruction-quality/${RUNMODE}" -name 'a=inner_nodes+*ext=.pqt' | grep -v "dsamp=0" | wc -l
+
+################################################################################
+echo Do collation
+################################################################################
+singularity exec docker://ghcr.io/mmore500/hstrat-reconstruction-quality:4a037849287dc258e92354aa09b7b2c13441c4f5 /bin/bash << 'EOF'
+set -e
+set -o nounset
+find "${HOME}/scratch/hstrat-reconstruction-quality/${RUNMODE}" -name 'a=inner_nodes*ext=.pqt' | grep -v 'dsamp=0' | python3 -m joinem --progress "${HOME}/scratch/hstrat-reconstruction-quality/${RUNMODE}/a=inner_nodes+dsamp=true+ext=.pqt"
+EOF
+
+################################################################################
+echo Finished
+################################################################################
+date
+echo "SECONDS ${SECONDS}"
